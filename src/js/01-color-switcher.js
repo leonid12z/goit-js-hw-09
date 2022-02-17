6ee6f3be508f12c4577bb4504e72de7b209af621
leonid12z <lelik_ceh@ukr.net>
2022-02-17 19:49:01 +0200

    add-files-dz-09

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,33 @@
+const refs = {
+   body: document.querySelector('body'),
+   startBut: document.querySelector('button[data-start]'),
+   stopBut: document.querySelector('button[data-stop]'),
+};
 
+let idTimer = null;
+
+refs.stopBut.disabled = true;
+refs.startBut.addEventListener('click', onStartBut);
+refs.stopBut.addEventListener('click', onStopBut);
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function onStartBut() {
+   idTimer = setInterval(() => {
+      refs.body.style.backgroundColor = getRandomHexColor();
+   }, 1000);
+
+   refs.startBut.disabled = true;
+   refs.stopBut.disabled = false;
+}
+
+function onStopBut() {
+   clearInterval(idTimer);
+
+   refs.startBut.disabled = false;
+   refs.stopBut.disabled = true;
+
+   console.log(` Interval with id ${idTimer} has stopped. `);
+}
