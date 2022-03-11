fcb6b937e6e73c98aa9931a26c36908af74d7c22
leonid12z <lelik_ceh@ukr.net>
2022-03-11 18:25:35 +0200

    add-files-09d/z

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,12 +1,40 @@
 // npm i notiflix
-import Notiflix from 'notiflix';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+// import Notiflix from 'notiflix';
+const form = document.querySelector('.form');
 
 
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    setTimeout(() => {
+      if (shouldResolve) {
+        // Fulfill
+        resolve({position, delay});
+      } else {
+        // Reject
+        reject({position, delay});
+      }
+    }, delay);
+  });
+}
+
+
+form.addEventListener('submit', createOnSubmit);
+
+function createOnSubmit(e) {
+  e.preventDefault();
+  let firstDelay = Number(e.target.elements.delay.value);
+  const amountValue = e.target.elements.amount.value;
+
+  for (let i = 1; i <= amountValue; i += 1) {
+    createPromise(i, firstDelay)
+      .then(({ position, delay }) => {
+        Notify.success(`Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        Notify.failure(`Rejected promise ${position} in ${delay}ms`);
+      });
+    firstDelay += Number(e.target.elements.step.value);
   }
 }
